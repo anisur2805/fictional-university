80aef6b22be6974d9ec41ce376be81f5f97471d6
Anisur Rahman <anisur2805@gmail.com>
2024-08-21 23:56:00 +0600

    create note obj

--- src/index.js
+++ src/index.js
@@ -4,8 +4,10 @@ import "../css/style.scss"
 import MobileMenu from "./modules/MobileMenu"
 import HeroSlider from "./modules/HeroSlider"
 import Search from './modules/Search'
+import Notes from './modules/Notes'
 
 // Instantiate a new object using our modules/classes
 const mobileMenu = new MobileMenu()
 const heroSlider = new HeroSlider()
 const search     = new Search()
+const notes = new Notes()
